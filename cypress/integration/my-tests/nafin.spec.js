115b6c1f64985f62489b64c38dd4e399cf21fc5b
Sergio Ugalde <serch037@gmail.com>
2020-05-22 22:01:53 -0500

    Fixed test style plus added assertions

--- cypress/integration/my-tests/nafin.spec.js
+++ cypress/integration/my-tests/nafin.spec.js
@@ -1,28 +1,30 @@
 describe("nafin.gob.mx", () => {
-    describe('Page loads', () => {
-        it('Sould complete use-case', () => {
-            cy.visit("https://www.nafin.com/portalnf/content/herramientas-de-negocio/simulador-de-creditos/simulador-de-creditos.do");
-            cy.get('#dispDate')
-                .type('05/05/2020');
-            cy.get('#btn').click(); // Hack to dismiss datepicker
-            cy.get('#creditAmount').clear().type("20000");
-            cy.get('#paymentMethod').select("Mensual");
+  it("Sould complete use-case", () => {
+    cy.visit(
+      "https://www.nafin.com/portalnf/content/herramientas-de-negocio/simulador-de-creditos/simulador-de-creditos.do"
+    );
+    cy.get("#dispDate").type("05/05/2020").should('have.value',"05/05/2020");
+    cy.get("#btn").click(); // Hack to dismiss datepicker
 
-            cy.get('#period').select("2 años");
-            cy.get('#rate').clear().type(15.0);
+    cy.get("#creditAmount").clear().type("20000").should('have.value',"20000");
 
-            cy.contains('Calcular').click()
+    cy.get("#paymentMethod").select("Mensual").should('have.value',"1");
 
-            cy.get('#encuestaWindow').then($modal => {
-            if ($modal.is(':visible')) {
-                cy.contains('Ahora no').click()
-            }
-            })
-            cy.get('.modal-encuesta-body')
-            .should('not.be.visible')
+    cy.get("#period").select("2 años").should('have.value', "2");
+    cy.get("#rate").clear().type(15.0).should('have.value', 15.0);
 
-            cy.get('#resultadosSimulador').get('tbody').find('tr').should('have.length', 24)
+    cy.contains("Calcular").click();
 
-        })
+    cy.get("#encuestaWindow").then(($modal) => {
+      if ($modal.is(":visible")) {
+        cy.contains("Ahora no").click();
+      }
     });
+    cy.get(".modal-encuesta-body").should("not.be.visible");
+
+    cy.get("#resultadosSimulador")
+      .get("tbody")
+      .find("tr")
+      .should("have.length", 24);
+  });
 });
